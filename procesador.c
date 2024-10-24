505b50bbf394ac474415d40e6aee58ef8694febc
DerekFGTez <fonseca_oliver376@comunidad.unam.mx>
2024-10-24 09:51:36 -0600

    cambios

--- procesador.c
+++ procesador.c
@@ -5,17 +5,17 @@
 
 void init_processor(Processor *cpu)
 {
-    memset(cpu->memory, 0, MEM_SIZE);
+    memset(cpu->memory, 0, MEM_SIZE * sizeof(int));
 
     /*cpu->registers[0] = 0;
     cpu->registers[1] = 0;*/
 
-    memset(cpu->registers, 0, NUM_REGISTERS);
+    memset(cpu->registers, 0, NUM_REGISTERS * sizeof(int));
 
     /*cpu->flags[ZF] = false;
     cpu->flags[NF] = false;*/
 
-    memset(cpu->flags, 0, NUM_FLAGS);    
+    memset(cpu->flags, 0, NUM_FLAGS * sizeof(int));    
 
     cpu->PC = 0;
 }
