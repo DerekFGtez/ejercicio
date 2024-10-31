596ff766d09301e52a61e5b018fa59dc6bb68fa8
DerekFGtez <fonseca_oliver376@comunidad.unam.mx>
2024-10-31 09:52:53 -0600

    Cambios

--- main.c
+++ main.c
@@ -1,53 +1,11 @@
-/*#include <stdio.h>
-#include <stdlib.h>
-#include <windows.h>
-#include "procesador.h"
-
-int main(int argc, char *argv[])
-{
-    SetConsoleOutputCP(CP_UTF8);
-
-    if (argc < 2)
-    {
-        fprintf(stderr, "Error: Se requiere el nombre del archivo de instrucciones.\n");
-        fprintf(stderr, "Uso: %s <archivo de instrucciones>\n", argv[0]);
-        return 1;
-    }
-
-    Processor cpu;
-    init_processor(&cpu); //Inicializa el procesador
-
-    load_program(&cpu, argv[1]); //Carga el programa desde el archivo
-
-    int max_iterations = 100;
-
-    while (cpu.PC < cpu.total_instructions && max_iterations > 0) //Bucle de la ejecución de cada instrucción
-    {
-        alu_and_control_unity(&cpu); //Ejecuta cada instrucción del archivo de texto
-        max_iterations--;
-    }
-
-    if (max_iterations == 0)
-    {
-        printf("Se alcanzó el límite máximo de iteraciones, posible bucle infinito\n");
-    }
-
-    fprint(&cpu); //Imprime el estado final del procesador
-
-    return 0;
-}*/
-
 #include <stdio.h>
 #include <stdlib.h>
-#include <windows.h>
 #include "procesador.h"
 
-FILE *output_file; // Variable global para el archivo de salida
+FILE *output_file; //Archivo de salida
 
 int main(int argc, char *argv[])
 {
-    SetConsoleOutputCP(CP_UTF8);
-
     Processor cpu;
     init_processor(&cpu);
 
@@ -80,6 +38,6 @@ int main(int argc, char *argv[])
         cpu.PC++;
     }
 
-    fclose(output_file); // Cierra el archivo de salida al final
+    fclose(output_file);
     return 0;
 }
